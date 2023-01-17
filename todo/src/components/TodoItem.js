ec3b6a14a29ff05dd737dc7b2ab4091b59958ea8
Marwa-MEZLINI <m.mezlini92@gmail.com>
2023-01-17 13:54:15 +0100

    styling the todo list

--- todo/src/components/TodoItem.js
+++ todo/src/components/TodoItem.js
@@ -10,18 +10,20 @@ function TodoItem(props) {
 
     return (
         <React.Fragment>
-            <div className="card">
-                <p className="h4"> {todo.title} </p>
-                <div className="form-check d-flex justify-self-end p-0"  >
-                    <input className="checkbox me-1" type="checkbox" onChange={() => { dispatch(isChecked(todo.id)) }} ></input>
-                    <label className={`form-check-label mx-1 ${todo.checked ? "text-decoration-line-through" : ""}`}>
-                        {todo.text}
-                    </label>
-                </div>
-                <div>
-                    <p> {todo.category} </p>
-                    <p> {todo.priority} </p>
-                    <button type="button" className="btn-close ms-auto" aria-label="Close" onClick={() => dispatch(removeTodo(todo.id))}></button>
+            <div className="card ps-3">
+                <p className="h4 ps-3 pt-3"> {todo.title} </p>
+                <div className="d-flex flex-row align-items-center py-3 justify-content-between">
+                    <div className="form-check d-flex p-0 ms-3 ml-auto"  >
+                        <input className="checkbox me-1" type="checkbox" onChange={() => { dispatch(isChecked(todo.id)) }} ></input>
+                        <label className={`form-check-label mx-1 align-center ${todo.checked ? "text-decoration-line-through" : ""}`}>
+                            {todo.text}
+                        </label>
+                    </div>
+                    <div className="d-flex flex-row justify-content-between px-3 ml-auto align-items-center">
+                        <p className="m-0 px-3"> <span className="fw-bold px-3 text-indigo-800">Category:  </span> {todo.category} </p>
+                        <p className="m-0 px-3"> <span className="fw-bold px-3">Priority:  </span> {todo.priority} </p>
+                        <button type="button" className="btn-close ms-auto  px-3" aria-label="Close" onClick={() => dispatch(removeTodo(todo.id))}></button>
+                    </div>
                 </div>
             </div>
         </React.Fragment>
